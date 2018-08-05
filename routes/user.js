e8640a6f4f8ff04827ebc395bcb09f4fe99698c6
shumar <shumar456@gmail.com>
2018-08-05 23:50:18 +0300

    reset password user work

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,7 @@ var isAuthenticated = function (req, res, next) {
 router.get("/", isAuthenticated, function(request, response) {
     User.find({_id:request.user._id}, function(err,result){
         if(err) return console.log(err);
-        console.log("USEROOO+",result);
+        //console.log("USEROOO+",result);
         response.render("user.hbs", {            
             user: request.user.name,
             email: request.user.email,
@@ -65,20 +65,8 @@ router.get("/pasedit/:pasref", isAuthenticated, function(request, response) {
 router.post("/pasedit/:pasref", bodyParser.urlencoded({extended: true}), function(request, response) {
     
     if(!request.body) return response.sendStatus(400);
-    //console.warn(request);   
-    var pasref = request.params["pasref"];
-    //response.render("notegood.hbs");
-   /* User.findOneAndUpdate(
-        {pasref: pasref}, // критерий выборки
-        { $set: {password: request.body.userpassword}}, // параметр обновления
-        function(err, result){
-            if(err) return console.log(err);
-            
-            console.log(result); 
-
-            response.send("<p>Пароль изменен</p><p><a href='http://localhost:3000/notes/'>Вернуться</a></p>");           
-        });
-    */
+      
+    var pasref = request.params["pasref"];   
    User.findOne({pasref: pasref}, function(err, user){      
     if(err) return console.log(err);
     user.password = request.body.userpassword;
@@ -88,14 +76,76 @@ router.post("/pasedit/:pasref", bodyParser.urlencoded({extended: true}), functio
     });
    // console.log("User reload==", user); 
              
-});   
+    });   
+    /* User.findOne({pasref: pasref}, function(err, result){
+        if(err) return console.log(err);
+        console.log("User reload==", result); 
+                 
+    });    */
+});
+router.get("/pasreset", function(request, response) {   
+    response.render("pasreset.hbs");    
+});
+
+router.post("/pasreset", bodyParser.urlencoded({extended: true}), function(request, response) { 
+    if(!request.body) return response.sendStatus(400);     
+    var username = request.body.username;
+    var pasref = crypto.randomBytes(20).toString('hex');
+    User.findOneAndUpdate(
+        {name:username}, // критерий выборки
+        { $set: {pasref:pasref}}, // параметр обновления
+        function(err, result){
+            if(err) return console.log(err);
+            // как отработать ошибку - нет такого пользователя           
+        console.log("UPDATE=",result);         
+        });      
+   User.findOne({name: username}, function(err, user){      
+    if(err) return console.log(err);
+    /* if(!user.email){
+        response.send("<p>Такого пользователя не существует</p><p><a href='/pasreset'>Вернуться</a></p>");
+    } */
+    sgMail.setApiKey(sendgridKey);
+    var msg3 = {
+    to: user.email,
+    from: "todo@example.com",
+    subject: "Восстановление пароля пользователя TODO2",
+    text:
+      "Чтобы создать новый пароль перейдите по ссылке http://localhost:3000/user/pasreset/" +
+      pasref,
+    html:
+      "Чтобы создать новый пароль перейдите по ссылке http://localhost:3000/user/pasreset/" +
+      pasref
+  };
+  sgMail.send(msg3);
+  response.send("<p>На почту отправлены инструкции для восстановления пароля</p><p><a href='http://localhost:3000/'>на главную</a></p>"); 
+    
+             
+    });   
     User.findOne({pasref: pasref}, function(err, result){
         if(err) return console.log(err);
         console.log("User reload==", result); 
                  
     });   
 });
-
+router.get("/pasreset/:pasref", function(request, response) {
+    var pasref = request.params["pasref"];
+    response.render("pasresetnew.hbs",{
+        pasref:pasref,
+    });    
+});
+router.post("/pasreset/:pasref", bodyParser.urlencoded({extended: true}), function(request, response) { 
+    var pasref = request.params["pasref"];   
+   User.findOne({pasref: pasref}, function(err, user){      
+    if(err) return console.log(err);
+    user.password = request.body.userpassword;
+    user.save((err) => {
+      if (err) { return next(err); }    
+      response.send("<p>Ваш новый пароль создан, для авторизации перейдите по ссылке</p><p><a href='/login/'>Вернуться</a></p>");
+    });
+    console.log("User reload==", user); 
+             
+    });   
+});
 
 
 module.exports = router;
